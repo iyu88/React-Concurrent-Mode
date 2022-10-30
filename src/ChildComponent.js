d4a99ec37930b4ff3f32c2024204859b3ba6542b
iyu88 <iyu88@naver.com>
2022-10-30 17:41:46 +0900

    feat: fetchThenRender 2 dataset 테스트

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from "react";
 import fetchThenRender from "./fetchThenRender";
 
-const photoData = fetchThenRender();
+const fetchDummyData = () => {
+  return Promise.all([
+    fetchThenRender("photos"),
+    fetchThenRender("comments"),
+  ]).then(([photos, comments]) => [photos, comments]);
+};
+
+const dummyData = fetchDummyData();
 
 export default function ChildComponent() {
-  const [dummyData, setDummyData] = useState([]);
+  const [dummyPhotoData, setDummyPhotoData] = useState([]);
+  const [dummyCommentData, setDummyCommentData] = useState([]);
 
   useEffect(() => {
-    photoData.then((response) => {
-      setDummyData(response.data);
+    dummyData.then(([photos, comments]) => {
+      setDummyPhotoData(photos.data);
+      setDummyCommentData(comments.data);
     });
   }, []);
 
   return (
     <div>
-      {dummyData.length ? (
-        dummyData.map((d) => <h4 key={d.id}>{JSON.stringify(d)}</h4>)
+      {dummyPhotoData.length ? (
+        dummyPhotoData.map((d) => <h4 key={d.id}>{JSON.stringify(d)}</h4>)
       ) : (
         <h1> 데이터 로딩 중...</h1>
       )}
-      <img src="./image.JPG" alt="테스트 이미지" width="100px" />
     </div>
   );
 }
--- src/fetchThenRender.js
+++ src/fetchThenRender.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const dataUrl = "https://jsonplaceholder.typicode.com/photos";
-const fetchThenRender = () => {
+const dataUrl = "https://jsonplaceholder.typicode.com/";
+const fetchThenRender = (path) => {
   return new Promise((resolve, reject) => {
-    const response = axios.get(dataUrl);
+    const response = axios.get(dataUrl + path);
     if (response) {
       resolve(response);
     } else {
